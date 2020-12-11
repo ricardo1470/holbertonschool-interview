102b4c85260e5878f3b6c51f7cfa3bcb8cc14879
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-12-10 20:56:28 -0500

    boxes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -3,4 +3,17 @@
 
 
 def canUnlockAll(boxes):
-    
+    """ boxes """
+    keys = {0}
+    added = False
+
+    while not added:
+        updated = False
+        for i in range(len(boxes)):
+            if i in keys:
+                for key in boxes[i]:
+                    if key not in keys:
+                        updated = True
+                    keys.add(key)
+        added = True if not updated else False
+    return len(keys) == len(boxes)
