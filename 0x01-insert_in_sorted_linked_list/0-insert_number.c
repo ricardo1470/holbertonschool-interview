4a87d3d12f92753abba6eb1fc201056215a3e067
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-12-17 03:27:09 -0500

    function in C that inserts a number into
    a sorted singly linked list

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -1,9 +1,10 @@
 #include "lists.h"
 
 /**
- * insert_node - insert node
+ * insert_node - inserts a number into a sorted singly linked list.
  * @head: pointer to head of list
  * @number: node to insert in list
+ * Return: Always 0.
  */
 listint_t *insert_node(listint_t **head, int number)
 {
