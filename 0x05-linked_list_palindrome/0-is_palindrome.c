16fe629e5030730ebc525cd7f3d76ff5c196d57b
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2021-01-21 20:02:59 -0500

    update

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -1,40 +1,6 @@
 #include "lists.h"
 
 /**
- * is_palindrome - Function to check if given linked list is
- * palindrome or not
- * @head: pointer to head linkedlist
- * Description: function that checks if a singly linked list is a palindrome.
- * Return - 0 if it is not a palindrome, 1 if it is a palindrome
- */
-int is_palindrome(listint_t **head)
-{
-	int length = list_length(head);
-	int half_length = length / 2;
-	int i, if_palindrome = 1;
-	listint_t *reversed_head;
-	listint_t *current = *head;
-	listint_t *current_reversed;
-
-	reversed_head = *head;
-	for (i = 0; i < half_length; i++)
-		reversed_head = reversed_head->next;
-	if (length % 2 != 0)
-		reversed_head = reversed_head->next;
-	reversed_head = reversed(&reversed_head);
-	current_reversed = reversed_head;
-	current = *head;
-	while (current_reversed != NULL)
-	{
-		if (current_reversed->n != current->n)
-			if_palindrome = 0;
-		current_reversed = current_reversed->next;
-		current = current->next;
-	}
-	return (if_palindrome);
-}
-
-/**
  * list_length - Get the length of a linked list
  * @head: pointer to head linkedlist
  * Return: lenght
@@ -70,4 +36,38 @@ listint_t *reversed(listint_t **head)
 	}
 	*head = prev;
 	return (*head);
+}
+
+/**
+ * is_palindrome - Function to check if given linked list is
+ * palindrome or not
+ * @head: pointer to head linkedlist
+ * Description: function that checks if a singly linked list is a palindrome.
+ * Return - 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	int length = list_length(head);
+	int half_length = length / 2;
+	int i, if_palindrome = 1;
+	listint_t *reversed_head;
+	listint_t *current = *head;
+	listint_t *current_reversed;
+
+	reversed_head = *head;
+	for (i = 0; i < half_length; i++)
+		reversed_head = reversed_head->next;
+	if (length % 2 != 0)
+		reversed_head = reversed_head->next;
+	reversed_head = reversed(&reversed_head);
+	current_reversed = reversed_head;
+	current = *head;
+	while (current_reversed != NULL)
+	{
+		if (current_reversed->n != current->n)
+			if_palindrome = 0;
+		current_reversed = current_reversed->next;
+		current = current->next;
+	}
+	return (if_palindrome);
 }
