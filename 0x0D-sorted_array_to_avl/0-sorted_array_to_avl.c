1351f59ede25b605264243338908b4e271717583
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2021-03-24 17:39:43 -0500

    create files

---
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * sorted_array_to_avl -
+ * @array: pointer to the first element of the array to be converted
+ * @size: the number of element in the array
+ * Return -  a pointer to the root node of the created AVL tree
+ **/
+avl_t *sorted_array_to_avl(int *array, size_t size)
+{
+	if (array == null)
+	{
+		return
+	}
+}
