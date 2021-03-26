b74abe1e60408b8b1c30a836ab754fe72bae1526
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2021-03-25 23:38:42 -0500

    create files

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -1,15 +1,69 @@
 #include "binary_trees.h"
 
 /**
- * sorted_array_to_avl -
+ * sorted_array_to_avl - binary tree AVL
  * @array: pointer to the first element of the array to be converted
  * @size: the number of element in the array
- * Return -  a pointer to the root node of the created AVL tree
+ * Return: a pointer to the root node of the created AVL tree
  **/
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
-	if (array == null)
+	avl_t *tree = NULL;
+
+	tree = recursive_tree(array, 0, (int)size - 1);
+	if (!tree)
 	{
-		return
+		return (NULL);
 	}
+	return (tree);
+}
+
+/**
+ * create_node - Creates a binary tree node
+ * @n: Value to put in the new node.
+ * Return: Pointer to the new node, or NULL on failure
+ */
+avl_t *create_node(int n)
+{
+	avl_t *node = NULL;
+
+	if (n == 0)
+		return (NULL);
+	node = malloc(sizeof(avl_t));
+	if (!node)
+		return (NULL);
+	node->parent = NULL;
+	node->left = NULL;
+	node->right = NULL;
+	node->n = n;
+	return (node);
+}
+
+/**
+ * recursive_tree - add node with recursive fuction
+ * @array: array.
+ * @ben: begin.
+ * @end: end.
+ * Return: tree
+ */
+avl_t *recursive_tree(int *array, int ben, int end)
+{
+	avl_t *left = NULL, *right = NULL, *parent = NULL;
+	size_t n = 0;
+
+	if (ben > end)
+		return (NULL);
+	n = (ben + end) / 2;
+	left = recursive_tree(array, ben, n - 1);
+	right = recursive_tree(array, n + 1, end);
+	parent = create_node(array[n]);
+	if (!parent)
+		return (NULL);
+	parent->left = left;
+	parent->right = right;
+	if (left)
+		left->parent = parent;
+	if (right)
+		right->parent = parent;
+	return (parent);
 }
--- 0x0D-sorted_array_to_avl/binary_trees.h
+++ 0x0D-sorted_array_to_avl/binary_trees.h
@@ -1,8 +1,10 @@
 #ifndef BINARY_TREES_H
-#define SBINARY_TREES_H
+#define BINARY_TREES_H
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -14,10 +16,10 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 /*Binary Search Tree*/
@@ -26,6 +28,10 @@ typedef struct binary_tree_s binary_tree_t;
 /*AVL Tree*/
 typedef struct binary_tree_s avl_t;
 
+/* Prototypes */
 avl_t *sorted_array_to_avl(int *array, size_t size);
+avl_t *recursive_tree(int *array, int ben, int end);
+avl_t *create_node(int n);
+void binary_tree_print(const binary_tree_t *tree);
 
 #endif /* SBINARY_TREES_H */
