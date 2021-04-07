95041dbc9d376894ecaadfe5dd398f560836a025
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2021-04-07 11:02:47 -0500

    create files

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -4,21 +4,21 @@ const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
 const start = function () {
-    request(url, function (error, response, body) {
-        if (error) throw error;
-        end(JSON.parse(body).characters, 0);
-    });
+  request(url, function (error, response, body) {
+    if (error) throw error;
+    end(JSON.parse(body).characters, 0);
+  });
 };
 
 const end = function (characters, i) {
-    if (characters.length === i) {
+  if (characters.length === i) {
     return;
-    }
-    request(characters[i], function (error, response, body) {
-        if (error) throw error;
-        console.log(JSON.parse(body).name);
-        end(characters, ++i);
-    });
+  }
+  request(characters[i], function (error, response, body) {
+    if (error) throw error;
+    console.log(JSON.parse(body).name);
+    end(characters, ++i);
+  });
 };
 
 start();
