b81a11dc5139e8292a8efe7894c78b4b74b57d42
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2021-04-15 17:01:20 -0500

    create files
    add function that indicating total amount of rainwater

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" a list of non-negative integers representing walls of width 1,
+    calculate how much water will be retained after it rains. """
+
+
+def rain(walls):
+    """
+    - walls is a list of non-negative integers.
+    - Return: Integer indicating total amount of rainwater retained.
+    - If the list is empty return 0."""
+    # To store the maximum water
+    max_water = 0
+
+    if len(walls) < 0:
+        return 0
+    for i in range(len(walls)):
+        left = walls[i]
+        for j in range(i):
+            left = max(left, walls[j])
+        right = walls[i]
+        for j in range(1 + i, len(walls)):
+            right = max(right, walls[j])
+        max_water += min(left, right) - walls[i]
+
+    return max_water
---
+++ 0x10-rain/README.md
@@ -0,0 +1,21 @@
+> # 0x10. Rain
+---
+| **Filename** | **Description** |
+|---|---|
+| [0. Rain](./0-rain.py) | a list of non-negative integers representing walls of width 1, calculate how much water will be retained after it rains. |
+|  |   |
+
+---
+> ## contact 💬
+
+| [twitter](https://twitter.com/RICARDO1470) | [linkedin](https://www.linkedin.com/in/ricardo-alfonso-camayo/) | [mail](1466@holbertonschool.com) | [github](https://github.com/ricardo1470/README/blob/master/README.md) |
+|---|---|---|---|
+
+---
+
+## License
+*`0x10. Rain` is open source and therefore free to download and use without permission.*
+
+<a href="url"><img src="https://www.holbertonschool.com/holberton-logo.png" align="middle" width="100" height="30"></a>
+
+---
