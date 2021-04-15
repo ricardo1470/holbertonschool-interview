85a6028ae18cba8b96b66c5abf7b5703f95bf342
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2021-04-15 17:42:20 -0500

    update files

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -10,21 +10,21 @@ def rain(walls):
     - If the list is empty return 0."""
     # To store the maximum water
     max_water = 0
-    len_arr = len(walls)
 
-    if len(walls < 0):
+    if len(walls) < 0:
         return 0
 
-    for i in range(1, len_arr -1):
+    for i in range(len(walls)):
         left = walls[i]
 
         for j in range(i):
-            left = max(left,walls[j])
+            left = max(left, walls[j])
+
         right = walls[i]
 
-        for j in range(i + 1, len_arr):
+        for j in range(1 + i, len(walls)):
             right = max(right, walls[j])
 
-        max_water += (min(left, right) - walls[i])
+        max_water += min(left, right) - walls[i]
 
     return max_water
