7e62bb65105029b0964f499f34f5ea5f648e316d
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2021-04-29 15:20:34 -0500

    create files

--- 0x12-advanced_binary_search/0-advanced_binary.c
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -1,52 +1,64 @@
 #include "search_algos.h"
 
 /**
- * advanced_binary - function that searches for a value in a sorted array of integers.
+ * print_array - print element to the array
+ * @array: is a pointer to the first element to the array
+ * @from: number of element in array
+ * @to: number of element in array
+ */
+void print_array(int *array, size_t from, size_t to)
+{
+	unsigned int i;
+
+	printf("Searching in array: ");
+	for (i = from; i < to; i++)
+	{
+		printf("%d, ", array[i]);
+	}
+	printf("%i\n", array[i]);
+}
+
+/**
+ * ad_binary - print the message
+ * @array: is a pointer to the first element of the array to search in array
+ * @left: index to start search
+ * @right: index to end search
+ * @value: value to find
+ *
+ * Return: index
+ */
+int ad_binary(int *array, size_t left, size_t right, int value)
+{
+	if (left <= right)
+	{
+		int mid = (left + right) / 2;
+
+		print_array(array, left, right);
+		if (array[mid] == value && array[mid - 1] != value)
+			return (mid);
+		else if (array[mid] >= value)
+			return (ad_binary(array, left, mid, value));
+		else
+			return (ad_binary(array, mid + 1, right, value));
+	}
+	return (-1);
+}
+
+/**
+ * advanced_binary - function that searches for a value in
+ * a sorted array of integers.
  * @array: is a pointer to the first element of the array to search in
  * @size: is the number of elements in array
- * Return - Always 0
+ * @value: is the value to search for
+ * Return: If value is not present in array or if array is NULL,
+ * your function must return -1
  **/
 int advanced_binary(int *array, size_t size, int value)
 {
-	int left = 0;
-	int i;
-	int right = size - 1;
-	int mid;
-
-    if (!value || array == NULL)
-    {
-        return (-1);
-    }
-
-	while (left <= right)
+	if (!value || array == NULL)
 	{
-		printf("Searching in array: ");
-		for (i = left ; i <= right; i++)
-		{
-			printf("%d", array[i]);
-			if (i < right)
-			{
-				printf(", ");
-			}
-			else
-			{
-				printf("\n");
-			}
+		return (-1);
 		}
-		mid = ((left + right) / 2);
 
-		if (array[mid] == value)
-		{
-			return (mid);
-		}
-		else if (array[mid] < value)
-		{
-			left = mid + 1;
-		}
-		else if (array[mid] > value)
-		{
-			right = mid - 1;
-		}
-	}
-	return (-1);
+	return (ad_binary(array, 0, size - 1, value));
 }
