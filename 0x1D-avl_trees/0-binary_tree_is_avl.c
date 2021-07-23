7b27c795d287cae60d535799d770673a22c6ae32
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2021-07-22 21:39:51 -0500

    create files

---
+++ 0x1D-avl_trees/0-binary_tree_is_avl.c
@@ -0,0 +1,59 @@
+#include "binary_trees.h"
+
+/**
+ * is_valid_avl - checks if a binary tree is a valid AVL tree
+ * @tree: The root of the binary tree
+ * @min: The minimum node in the tree
+ * @max: The maximum node in the tree
+ * @height: The height of the tree
+ * Returns: true if the binary tree is AVL, false otherwise
+ **/
+int is_valid_avl(const binary_tree_t *tree, int min, int max, int *height)
+{
+	if (tree == NULL)
+	{
+		return (1);
+	}
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		if (min <= tree->n && tree->n <= max)
+		{
+			*height = 1;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	else
+	{
+		int left_height, right_height;
+
+		if (tree->left == NULL || tree->right == NULL)
+		{
+			return (0);
+		}
+		if (!is_valid_avl(tree->left, min, tree->n, &left_height) ||
+		!is_valid_avl(tree->right, tree->n, max, &right_height))
+		{
+			return (0);
+		}
+		if (left_height != right_height)
+		{
+			return (0);
+		}
+		*height = left_height + 1;
+	}
+}
+
+/**
+ * binary_tree_is_avl - Returns true if the binary tree is AVL
+ * @tree: The root of the binary tree
+ * Returns: true if the binary tree is AVL, false otherwise
+ **/
+int binary_tree_is_avl(const binary_tree_t *tree)
+{
+	int height;
+
+	return (is_valid_avl(tree, -1, INT_MAX, &height));
+}
---
+++ 0x1D-avl_trees/README.md
@@ -0,0 +1,22 @@
+> # 0x1D. AVL Trees
+---
+
+| **Filename** | **Description** |
+|---|---|
+| [0. Is AVL](./0-binary_tree_is_avl.c) | function that checks if a binary tree is a valid AVL Tree |
+|  |   |
+
+---
+> ## contact 💬
+
+| [twitter](https://twitter.com/RICARDO1470) | [linkedin](https://www.linkedin.com/in/ricardo-alfonso-camayo/) | [mail](1466@holbertonschool.com) | [github](https://github.com/ricardo1470/README/blob/master/README.md) |
+|---|---|---|---|
+
+---
+
+## License
+*`0x1D. AVL Trees` is open source and therefore free to download and use without permission.*
+
+<a href="url"><img src="https://www.holbertonschool.com/holberton-logo.png" align="middle" width="100" height="30"></a>
+
+---
